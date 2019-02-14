e349424cc9291cb0beb4a32533f891fd48885ef5
Austin Joo <joojinhyung@gmail.com>
2019-02-14 14:52:32 -0500

    Working on deploying app

--- db/db.js
+++ db/db.js
@@ -6,10 +6,10 @@ const {Pool} = require('pg');
 
 const pool = new Pool({
     user: process.env.PG_USER || 'postgres',
-    password: process.env.PG_PW || 'Aj06231997...',
-    host: process.env.PG_HOST || '52.14.75.54',
+    password: process.env.PG_PW || '',
+    host: process.env.PG_HOST || 'localhost',
     database: process.env.PG_DATABASE || 'custompokemon',
     port: process.env.PG_PORT || 5432
-});
+  });
 
 module.exports = pool;
